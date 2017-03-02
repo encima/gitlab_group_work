0c9e05fa33dd5f4334fa5c4325bc7c0e4dec5925
encima <encima@gmail.com>
2017-03-02 19:32:47 +0000

    modified group clone to inspect repos (expects gitinspector to be installed

--- group_clone.rb
+++ group_clone.rb
@@ -4,7 +4,8 @@ require 'yaml'
 config = YAML::load_file('gitlab.yaml')
 puts config
 source_dir = config["directory"]
-group = config["group"]
+MODULE = config["module"]
+group = "#{MODULE}_project"
 
 # set a user private token
 Gitlab.private_token = config["Gitlab"]["token"]
@@ -19,21 +20,22 @@ user = g.user
 # get a user's email
 puts "Welcome, #{user.email}. Just checking your groups now..."
 
-groups = Gitlab.groups(per_page:5)
-groups.auto_paginate do |group|
-	if group.name == config["group"]
-		puts "#{group.name} found"
-		for p in Gitlab.group_projects(group.name)
-			puts "Cloning #{p.name}"
-			puts p.http_url_to_repo
-			begin
-				#using exec exits the current process, always use system!
-				system("git clone #{p.http_url_to_repo} #{source_dir + p.name}")
-			rescue Exception => e
-				next
-			end
-		end
+# groups = Gitlab.groups(per_page:5)
+# groups.auto_paginate do |group|
+# 	if group.name == config["group"]
+# 		puts "#{group.name} found"
+for p in Gitlab.group_projects(group)
+	puts "Inspecting #{p.name}"
+	puts "URL: " + p.http_url_to_repo
+	begin
+		#using exec exits the current process, always use system!
+		system("gitinspector -HTlrm #{p.http_url_to_repo}")
+	rescue Exception => e
+		next
 	end
+	puts "------------------"
 end
+# 	end
+# end
 
 
